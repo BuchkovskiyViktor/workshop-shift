5844c0373c6ed931f10add32d57957aa246e8338
BuchkovskiyViktor <104356551+BuchkovskiyViktor@users.noreply.github.com>
2026-09-25 21:09:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BAD_WORKSHOP_CONFIG = {
-  supabaseUrl: 'PASTE_SUPABASE_PROJECT_URL_HERE',
-  supabasePublishableKey: 'PASTE_SUPABASE_PUBLISHABLE_KEY_HERE'
+  supabaseUrl: 'https://uhxamiqsksxyievujgaq.supabase.co',
+  supabasePublishableKey: 'sb_publishable_uttoHv3rujvdy61Buk0rMQ_mnsSdNI1'
 };
